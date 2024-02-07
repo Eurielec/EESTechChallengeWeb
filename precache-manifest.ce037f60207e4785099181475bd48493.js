dd875eae39ffbbea4670caa5d56e67ce92e5ff0d
GerardoZapicoLópezNeira <gerardozapico2002@gmail.com>
2024-02-07 16:11:40 +0100

    Updates

---
+++ precache-manifest.ce037f60207e4785099181475bd48493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1933523e9ace983ef8fd3f5af33773",
+    "url": "/React-Landing-Page-EC/index.html"
+  },
+  {
+    "revision": "b6bd372b4af4e568bddf",
+    "url": "/React-Landing-Page-EC/static/css/main.e588991f.chunk.css"
+  },
+  {
+    "revision": "72f6c702d1f8906d154e",
+    "url": "/React-Landing-Page-EC/static/js/2.c1f29ff8.chunk.js"
+  },
+  {
+    "revision": "b6bd372b4af4e568bddf",
+    "url": "/React-Landing-Page-EC/static/js/main.9df02937.chunk.js"
+  },
+  {
+    "revision": "9a8efb99be4592ac51a7",
+    "url": "/React-Landing-Page-EC/static/js/runtime~main.9499dc0a.js"
+  }
+]);
